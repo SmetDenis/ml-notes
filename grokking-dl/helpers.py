b394502a52745c86b5929b190675d3814ca74749
SmetDenis <smet.denis@gmail.com>
2025-03-14 02:00:12 +0400

    mlp 4

--- grokking-dl/helpers.py
+++ grokking-dl/helpers.py
@@ -1,4 +1,3 @@
-
 def nf(number, decimals=6, dec_point='.', thousands_sep=','):
     num_parts = f"{number:.{decimals}f}".split(".")
     num_parts[0] = f"{int(num_parts[0]):,}".replace(",", thousands_sep)
